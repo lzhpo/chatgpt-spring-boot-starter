12b028489f05a91bfdfa3691a7c44b1d7a88a941
lzhpo <lzhpo1024@gmail.com>
2023-04-26 23:08:54 +0800

    Prefect OpenAiKeyWrapper

--- src/test/java/com/lzhpo/chatgpt/OpenAiKeyProviderTest.java
+++ src/test/java/com/lzhpo/chatgpt/OpenAiKeyProviderTest.java
@@ -26,13 +26,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 /**
  * @author lzhpo
  */
 @SpringBootTest
-// @Import({InnerOpenAiKeyProvider.class})
+@Import({InnerOpenAiKeyProvider.class})
 class OpenAiKeyProviderTest {
 
     @Autowired
